88019b367f914ef533caddf619e25ba9364470dd
Chinedu Alex <nwekealexchinedu@gmail.com>
2020-05-06 21:54:23 +0100

    Updates

---
+++ precache-manifest.1391131a128e970c2c97341a7bc3e3b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df20935de9cdd174f68fb3287ae52528",
+    "url": "/star-match-game/index.html"
+  },
+  {
+    "revision": "7820e99da011c1ea8cac",
+    "url": "/star-match-game/static/css/main.98c8bcba.chunk.css"
+  },
+  {
+    "revision": "e2876e94349e6adb0a35",
+    "url": "/star-match-game/static/js/2.53848e47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-match-game/static/js/2.53848e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7820e99da011c1ea8cac",
+    "url": "/star-match-game/static/js/main.3f76f72b.chunk.js"
+  },
+  {
+    "revision": "fecde215d4299b23d674",
+    "url": "/star-match-game/static/js/runtime-main.3542f6ed.js"
+  }
+]);
